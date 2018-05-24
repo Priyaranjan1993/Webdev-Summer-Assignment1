91131f20a9d95b1aafcee2b9740ffe33981dff63
Priyaranjan1993 <priyaranjan9090@gmail.com>
2018-05-24 16:39:35 -0400

    initial directory

---
+++ Webdev-Summer-Assignment1/src/main/webapp/jquery/models/user.model.client.js
@@ -0,0 +1,78 @@
+function User(username, password, firstName, lastName,phone,role,dateOfBirth) {
+	this.username = username;
+	this.password = password;
+	this.email = email;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.phone = phone;
+	this.role = role;
+	this.dateOfBirth = dateOfBirth;
+
+	this.setUsername = setUsername;
+	this.getUsername = getUsername;
+	this.setPassword = setPassword;
+	this.getPassword = getPassword;
+	this.setEmail = setEmail;
+	this.getEmail = getEmail;
+	this.setFirstName = setFirstName;
+	this.getFirstName = getFirstName;
+	this.setLastName = setLastName;
+	this.getLastName = getLastName;
+	this.setPhone = setPhone;
+	this.getPhone = getPhone;
+	this.setRole = setRole;
+	this.getRole = getRole;
+	this.setDateOfBirth = setDateOfBirth;
+	this.getDateOfBirth = getDateOfBirth;
+
+	function setUsername(username) {
+		this.username = username;
+	}
+	function getUsername() {
+		return this.username;
+	}
+	function setPassword(username) {
+		this.username = username;
+	}
+	function getPassword() {
+		return this.username;
+	}
+	function setEmail(username) {
+		this.username = username;
+	}
+	function getEmail() {
+		return this.username;
+	}
+	function setFirstName(username) {
+		this.username = username;
+	}
+	function getFirstName() {
+		return this.username;
+	}
+	function setLastName(username) {
+		this.username = username;
+	}
+	function getLastName() {
+		return this.username;
+	}
+	function setPhone(username) {
+		this.username = username;
+	}
+	function getPhone() {
+		return this.username;
+	}
+	function setRole(username) {
+		this.username = username;
+	}
+	function getRole() {
+		return this.username;
+	}
+	function setDateOfBirth(username) {
+		this.username = username;
+	}
+	function getDateOfBirth() {
+		return this.username;
+	}
+	
+
+
